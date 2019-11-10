5e323073772e3cd02359b308b4e0319b4fee9f2c
Emil Østergaard <isdragon112@gmail.com>
2019-11-10 18:43:13 +0100

    Got serverside rendered data with apollo up and running

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,44 @@
 import React from 'react'
 import Head from 'next/head'
 import styled from 'styled-components'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import { withApollo } from '../lib/Apollo'
 import Layout from '../components/Layout'
 import { CategoriHeader } from '../components/style'
 
-const Home = () => (
-  <Layout>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const HELLO_QUERY = gql`
+  query HelloQeury {
+    getBooks {
+      title
+    }
+  }
+`
 
-    <h1>I want to be a todo app some day!</h1>
-    <StyledTest>Im styled</StyledTest>
-    <CategoriHeader>Teeest</CategoriHeader>
-  </Layout>
-)
+const Home = () => {
+  const { data, loading, error } = useQuery(HELLO_QUERY)
+
+  if (loading) return <div />
+
+  console.log(data)
+
+  return (
+    <Layout>
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>{data.getBooks[0].title}</h1>
+      <StyledTest>Im styled</StyledTest>
+      <CategoriHeader>Teeest</CategoriHeader>
+    </Layout>
+  )
+}
 
 const StyledTest = styled.h2`
   color: red;
   background: blue;
 `
 
-export default Home
+export default withApollo(Home)
